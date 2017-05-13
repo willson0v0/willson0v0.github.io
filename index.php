c642e6615b53e03c5382611d26f9810ca0d70d2e
Willson Lee <willsonlgz@gmail.com>
2017-05-13 12:56:03 +0000

    ico added

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-Hello World!
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="zh-cn">
+    <head>
+        <meta name="baidu-site-verification" content="vfVmm1iEDk" />
+        <meta charset='utf-8'>
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <meta name="description" content="Voyager : Voyager-SFLS FRC">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Voyager</title>
+        <link rel="stylesheet" href="/mdl/material.min.css">
+        <link rel="stylesheet" href="/mdl/materialICO.css">
+        <link rel="shortcut icon" type="image/x-icon" href="/images/heart.ico" media="screen" />
+        <script type="text/javascript" src="/js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="/js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+        <script async src="/mdl/material.min.js"></script>
+        <script async type="text/javascript" src="/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    </body>
+</html>
